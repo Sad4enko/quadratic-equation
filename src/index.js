f5b710ca7b8ec80043569160f44d4d2aca01d273
Sad4enko <Sad4enko@mail.ru>
2018-02-27 01:17:28 +0400

    Quadratic_equation/feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var arr = equation.split('*');
+  arr[1] = arr[1].replace('x^2','');
+  arr[2] = arr[2].replace('x','');
+  arr[0] = arr[0].replace(/ /g,'');
+  arr[1] = arr[1].replace(/ /g,'');
+  arr[2] = arr[2].replace(/ /g,'');
+  var discriminant = Math.sqrt(+arr[1]*+arr[1] - 4*+arr[0]*+arr[2]);
+  var solves = [];
+  solves[0] = Math.round((-discriminant - +arr[1])/(2*+arr[0]));
+  solves[1] = Math.round((+discriminant - +arr[1])/(2*+arr[0]));
+  return solves.sort(compareNumbers);
+}
+function compareNumbers(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
 }
